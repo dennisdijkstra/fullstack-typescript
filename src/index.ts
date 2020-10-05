cfc7a6385adf8ceb3784c4e2286eecb473b3e5e9
dennisdijkstra <mail@dennisdijkstra.com>
2020-10-05 23:07:36 +0200

    set up mikro orm

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log('hello world');
+import { MikroORM } from '@mikro-orm/core';
+import { __prod__ } from './constants';
+import microConfig from './mikro-orm.config';
+
+const main = async () => {
+    const orm = await MikroORM.init(microConfig);
+    orm.getMigrator().up();
+};
+
+main().catch((err) => {
+    console.error(err);
+});
