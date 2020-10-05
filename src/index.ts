fa2398b9392c14c3aba2df0a509a07dd7785e68f
dennisdijkstra <mail@dennisdijkstra.com>
2020-10-05 23:43:34 +0200

    setup apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
 import 'reflect-metadata';
+import { PostResolver } from './resolvers/post';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import microConfig from './mikro-orm.config';
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     orm.getMigrator().up();
 
     const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [PostResolver],
+            validate: false,
+        }),
+        context: () => ({ em: orm.em }),
+    });
+
+    apolloServer.applyMiddleware({ app });
 
     app.listen(4000, () => {
         console.log('server started on localhost:4000');
