19ab633f432b1578beaa11c68c4502ad88a8a2f9
dennisdijkstra <mail@dennisdijkstra.com>
2020-10-05 23:39:57 +0200

    setup express web server

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import { Post } from './entities/Post';
 import 'reflect-metadata';
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import microConfig from './mikro-orm.config';
+import express from 'express';
 
 const main = async () => {
     const orm = await MikroORM.init(microConfig);
     orm.getMigrator().up();
 
-    const post = orm.em.create(Post, { title: 'my first post' });
-    await orm.em.persistAndFlush(post);
+    const app = express();
 
-    // const posts = await orm.em.find(Post, {});
-    // console.log(posts);
+    app.listen(4000, () => {
+        console.log('server started on localhost:4000');
+    });
 };
 
 main().catch((err) => {
