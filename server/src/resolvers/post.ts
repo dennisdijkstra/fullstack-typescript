9bce60421f93f04fe9bf20417e963b6712554c95
dennisdijkstra <mail@dennisdijkstra.com>
2020-10-12 21:38:42 +0200

    create post query and createPost mutation

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,10 +1,31 @@
+import { MyContext } from './../types';
 import { Post } from '../entities/Post';
-import { Resolver, Query } from 'type-graphql';
+import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql';
 
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
-    async posts(): Promise<Post[]> {
+    async posts(
+        @Ctx() { req, res }: MyContext
+     ): Promise<Post[]> {
+        console.log(req);
+        console.log(res);
         return Post.find();
+    }
+
+    @Query(() => Post, { nullable: true })
+    post(
+        @Arg('id', () => Int) id: number
+     ): Promise<Post | undefined> {
+        return Post.findOne(id);
+    }
+
+    @Mutation(() => Post)
+    async createPost(
+        @Arg('title') title: string
+     ): Promise<Post> {
+        return Post.create({
+            title,
+        }).save();
     }
 }
