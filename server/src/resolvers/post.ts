594df4513b4e60ac4eae8fc8e3ebfc3a42e35cf8
dennisdijkstra <mail@dennisdijkstra.com>
2020-10-14 20:14:26 +0200

    create updatePost

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -1,15 +1,14 @@
-import { MyContext } from './../types';
+// import { MyContext } from './../types';
 import { Post } from '../entities/Post';
 import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql';
+import { getConnection } from 'typeorm';
 
 @Resolver()
 export class PostResolver {
     @Query(() => [Post])
     async posts(
-        @Ctx() { req, res }: MyContext
+        // @Ctx() { req, res }: MyContext
      ): Promise<Post[]> {
-        console.log(req);
-        console.log(res);
         return Post.find();
     }
 
@@ -23,6 +22,30 @@ export class PostResolver {
     @Mutation(() => Post)
     async createPost(
         @Arg('title') title: string
+     ): Promise<Post> {
+        return Post.create({
+            title,
+        }).save();
+    }
+
+    @Mutation(() => Post, { nullable: true})
+    async updatePost(
+        @Arg('id') id: number,
+        @Arg('title') title: string
+     ): Promise<Post | null> {
+        const result = await getConnection()
+            .createQueryBuilder()
+            .update(Post)
+            .set({ title })
+            .where("id = :id", { id })
+            .execute();
+
+            return result.raw[0];
+    }
+
+    @Mutation(() => Post)
+    async deletePost(
+        @Arg('id') title: string
      ): Promise<Post> {
         return Post.create({
             title,
