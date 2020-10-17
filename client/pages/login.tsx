6c39359feba92b38a9c8d9808d1d373ecb1a709b
dennisdijkstra <mail@dennisdijkstra.com>
2020-10-17 17:52:09 +0200

    add initial register and login pages

---
+++ client/pages/login.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    console.log('register');
+
+    return (
+        <h1>Login</h1>
+    )
+};
+
+export default Login;
---
+++ client/pages/register.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Register = () => {
+    console.log('register');
+
+    return (
+        <h1>Register</h1>
+    )
+};
+
+export default Register;
